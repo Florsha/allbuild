bc8cbb9b73c7947cb12c43c66dfa1160efe81ec3
alexjumaoas <jumaoasalex95@gmail.com>
2025-09-10 18:21:59 +0800

    update map

---
+++ resources/js/Pages/Services/Location.jsx
@@ -0,0 +1,10 @@
+export default function Location() {
+  return (
+    <div className="space-y-4">
+      <h3 className="text-2xl font-bold text-gray-900">
+        Map Location
+      </h3>
+      
+    </div>
+  );
+}
