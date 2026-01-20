11f441a2e214cbda59c463150e579fd965cb7289
KMLopez2021 <karlmichael.lopez2021@gmail.com>
2026-01-20 17:35:30 +0800

    pushed update for tracker

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ use App\Http\Controllers\VideoTestimonialController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IDVerificationController;
-use App\Http\Controllers\Admin\AdminController; 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\TrackingController;
 use App\Models\VideoTestimonial;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
@@ -110,6 +111,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 Route::get('/id-verification', [IDVerificationController::class, 'index'])->name('id.verification');
 Route::post('/id-verification/process', [IDVerificationController::class, 'process'])->name('id.verification.process');
+Route::get('/tracking', [TrackingController::class, 'index'])->name('tracking.index');
+
 
 
 
